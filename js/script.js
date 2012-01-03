2d956ec75c0cb6bb9ec8051f99977f8f7271ff21
Andrew Appleton <andrew@mintdigital.com>
2012-01-03 09:53:11 +0000

    Adds JS module template

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ Contents:
 */
 
 /************************ 1. Global Scripts  ************************************/
-/* Global object - all modules live under this */
+// Global object - all modules live under this
 window.App = window.App || {};
 
-/* Scope scripts to an individual page by <html> element id */
+// Scope scripts to an individual page by <html> element id
 App.htmlID = function(id, fn){
   $(function(){
     var htmlID = App.htmlID;
@@ -25,5 +25,37 @@ App.htmlID = function(id, fn){
 };
 
 /************************ 2. Modules  *******************************************/
+// All modules to follow this pattern
+App.ModuleName = function(){
+  var publics, privates;
+
+  //Define private methods and variables
+  privates = {};
+
+  //Constructor
+
+  //Define public methods and variables
+  publics = {};
+  return publics;
+};
+
+// Module inheritance to follow this pattern
+App.SubModule = function(){
+  var publics, privates;
+
+  //Define private methods and variables
+  privates = {};
+
+  //Constructor - inherits from App.ClassName
+  publics = App.ModuleName();
+
+  //Define additional public methods and variables
+  publics.methodName = function(){};
+  return publics;
+};
 
 /************************ 3. Pages  *********************************************/
+// Everything in this section scoped to <html> ID
+App.htmlID('index', function(){
+  var myModule = App.SubModule();
+});
